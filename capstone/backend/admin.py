1f5a8c0cf40672853faf001144d1c653ca9f9766
keenntot2 <131374918+keenntot2@users.noreply.github.com>
2024-04-06 03:12:01 +0800

    [Backend] Create model for "languages"

--- capstone/backend/admin.py
+++ capstone/backend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Language
 
 # Register your models here.
+class LanguageAdmin(admin.ModelAdmin):
+    list_display=('language',
+                  'slug')
+    
+admin.site.register(Language, LanguageAdmin)
--- capstone/backend/models.py
+++ capstone/backend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Language(models.Model):
+    language = models.CharField(max_length=64)
+    slug = models.TextField(unique=True, blank=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.language)
+        super(Language,self).save(*args, **kwargs)
