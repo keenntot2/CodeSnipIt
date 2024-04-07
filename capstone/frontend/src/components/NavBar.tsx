90f38ab4d9cd8f9808f4b5281f0cbc7c9de699aa
keenntot2 <131374918+keenntot2@users.noreply.github.com>
2024-04-07 10:16:56 +0800

    navigation bug fixed

--- capstone/frontend/src/components/NavBar.tsx
+++ capstone/frontend/src/components/NavBar.tsx
@@ -1,36 +1,27 @@
 import { Button, HStack, Spinner, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import ColorModeSwitch from "../components/ColorModeSwitch";
+import { Navigate } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
 import useUserStore from "../hooks/useUserStore";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
+  const { mutate, isSuccess, isPending } = useLogout();
   const user = useUserStore((s) => s.user);
-  const { mutate, isSuccess } = useLogout();
-  const [logout, setLogout] = useState(false);
-
-  const navigate = useNavigate();
 
   if (isSuccess) {
-    navigate("/login");
     sessionStorage.removeItem("isLoggedIn");
+    return <Navigate to="/login" />;
   }
 
   if (!user) return null;
-
-  const handleClick = () => {
-    mutate(null);
-    setLogout(true);
-  };
 
   return (
     <HStack justifyContent={"space-between"}>
       <Text>Hi, {`${user.first_name} ${user.last_name}`}</Text>
       <HStack>
         <ColorModeSwitch />
-        <Button variant="ghost" onClick={handleClick}>
-          {logout ? <Spinner /> : "Logout"}
+        <Button variant="ghost" onClick={() => mutate(null)}>
+          {isPending ? <Spinner /> : "Logout"}
         </Button>
       </HStack>
     </HStack>
--- capstone/frontend/src/pages/Layout.tsx
+++ capstone/frontend/src/pages/Layout.tsx
@@ -1,22 +1,20 @@
 import { Box } from "@chakra-ui/react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
 import useRefreshToken from "../hooks/useRefreshToken";
 import useUser from "../hooks/useUser";
 import useUserStore from "../hooks/useUserStore";
-import NavBar from "./NavBar";
 
 const Layout = () => {
   const { data, isError, isLoading, isSuccess } = useUser();
   const setUser = useUserStore((s) => s.setUser);
   useRefreshToken(isSuccess);
 
-  const navigate = useNavigate();
-
   if (isLoading) return null;
   if (isError) {
     if (sessionStorage.getItem("isLoggedIn"))
       sessionStorage.removeItem("isLoggedIn");
-    navigate("/login");
+    return <Navigate to="/login" />;
   }
   if (isSuccess) {
     setUser(data);
