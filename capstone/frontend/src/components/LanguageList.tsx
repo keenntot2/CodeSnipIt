51699d52a5cb1ef90841efb07a3c78860f565106
keenntot2 <131374918+keenntot2@users.noreply.github.com>
2024-04-10 12:49:29 +0800

    Create language list accordion

--- capstone/frontend/src/components/LanguageList.tsx
+++ capstone/frontend/src/components/LanguageList.tsx
@@ -1,5 +1,13 @@
-import { Button, VStack } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+} from "@chakra-ui/react";
 import useLanguage from "../hooks/useLanguage";
+
+import { FaAngleDown } from "react-icons/fa";
 
 const LanguageList = () => {
   const { data, isError } = useLanguage();
@@ -7,13 +15,20 @@ const LanguageList = () => {
   if (isError) return null;
 
   return (
-    <VStack alignItems={"start"}>
+    <Accordion allowToggle>
       {data.results.map((language) => (
-        <Button key={language.id} size={"sm"} variant={"ghost"}>
-          {language.language}
-        </Button>
+        <AccordionItem key={language.id} borderRadius={5}>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left" paddingBlock={2}>
+              {language.language}
+            </Box>
+            <FaAngleDown />
+          </AccordionButton>
+
+          <AccordionPanel ml={5}>{language.slug}</AccordionPanel>
+        </AccordionItem>
       ))}
-    </VStack>
+    </Accordion>
   );
 };
 
