4aa9ad1d937399124b781cbf09d1ab5f4c2bc555
keenntot2 <131374918+keenntot2@users.noreply.github.com>
2024-05-16 15:47:06 +0800

    secure secret key and debug configuration settings

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pkil8!rqxujde1zy2nz6a-)(k#9_x$3!*h6+&#ry)e=-l41-lz'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
